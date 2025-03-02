23390d42731cbbc161d9755a3d1427354b3479fd
ahmed10mohsen74 <106989484+ahmed10mohsen74@users.noreply.github.com>
2025-03-03 01:22:37 +0200

    ahmed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-
+import Ahmed from './components/cards/card';
 
 function App() {
   
 
   return (
     <>
-      
+      <Ahmed />
       <h1>helllllllooooo  </h1>
     </>
   )
---
+++ src/components/cards/card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Ahmed = () => {
+    return(
+        <h1>ahmed mohsen</h1>
+    )
+} 
+export default Ahmed;
